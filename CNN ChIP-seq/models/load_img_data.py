54e08745ee9624a54c4fee2652373878d43df9e4
Luke Jones <55932367+sp00ks-L@users.noreply.github.com>
2021-09-01 16:11:23 +0100

    Adding the model

---
+++ CNN ChIP-seq/models/load_img_data.py
@@ -0,0 +1,45 @@
+# Title      :	load_img_data.py
+# Objective  :	Create a set of functions for loading and standardising the image data that needs to be loaded for training a CNN
+# Created by :	Luke
+# Created on :	Tue 06/07/21 14:19
+
+
+from os import walk
+
+import numpy as np
+from tensorflow.python.keras.preprocessing.image import img_to_array, load_img
+
+
+def load_data(dir, img_height, img_width):
+    print("Loading Images")
+    num_observations = len(next(walk(dir + '/Binding'))[2])
+    classes = ['/Binding/', '/Non-binding/']
+    # Pre-allocate
+    x = []
+    y = np.zeros((num_observations*2, 1))
+    # Pre-shuffle, we know the first part of the array will only be binding imgs
+    # Therefore we can set the first 'num_observations' = 1
+    y[:num_observations] = 1
+
+    for c in (classes):
+        print(f"Starting {c[1:-1]} imgs")
+        tmp_path = dir + c
+        img_list = next(walk(tmp_path))[2]
+
+        # Re-scale img values from 0-255 to 0.0-1.0
+        for i in range(num_observations):
+            img = load_img(tmp_path + img_list[i])
+            img = img.resize((img_height, img_width))
+            img_arr = img_to_array(img)
+            img_arr /= 255.0
+            x.append(img_arr)
+
+    x = np.array(x)
+    # Shuffle each array
+    rnd_indices = np.arange(num_observations * 2)
+    np.random.shuffle(rnd_indices)
+    x = x[rnd_indices]
+    y = y[rnd_indices]
+
+    print(f"Loaded {2*num_observations} images")
+    return x, y
